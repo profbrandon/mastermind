7b67a3b48ee0887f32595dbc9e0cc0452bf53535
Brandon Harrington <bdharrington0@outlook.com>
2024-01-09 14:47:42 -0600

    added comments to and refactored the MediaLoader class

--- src/MediaLoader.java
+++ src/MediaLoader.java
@@ -6,31 +6,33 @@ import java.util.Optional;
 import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 
+/**
+ * Singleton instance class for loading and retrieving media for the {@link Mastermind} game.
+ */
 public class MediaLoader {
-
-    public enum ImageType{
-        ICON,
-        MAIN_MENU,
-        SETTINGS_MENU
-    }
-
     private static final MediaLoader INSTANCE = new MediaLoader();
 
-    private Optional<Image> icon              = Optional.empty();
-    private Optional<Image> mainMenuImage     = Optional.empty();
-    private Optional<Image> settingsMenuImage = Optional.empty();
-    private Optional<Media> soundtrack        = Optional.empty();
-    private Optional<URL>   cssUrl            = Optional.empty();
+    private Optional<Media> soundtrack = Optional.empty();
+    private Optional<URL>   cssUrl     = Optional.empty();
 
+    /**
+     * Private constructor for building the singleton instance.
+     */
     private MediaLoader() {
-        this.icon              = loadFile("images/icon.png").map(Image::new);
-        this.mainMenuImage     = loadFile("images/main_menu.png").map(Image::new);
-        this.settingsMenuImage = loadFile("images/settings_menu.png").map(Image::new);
+        ImageType.ICON.image          = loadFile("images/icon.png").map(Image::new);
+        ImageType.MAIN_MENU.image     = loadFile("images/main_menu.png").map(Image::new);
+        ImageType.SETTINGS_MENU.image = loadFile("images/settings_menu.png").map(Image::new);
 
         this.soundtrack = loadResource("audio/mastermind.wav").map(audioURL -> new Media(audioURL.toExternalForm()));
         this.cssUrl     = loadResource("styling/textures.css");
     }
 
+    /**
+     * Private method for obtaining a {@link URL} to the requested resource.
+     * 
+     * @param location the relative path string within the resources folder
+     * @return the {@link Optional}<{@link URL}> to the requested resource
+     */
     private Optional<URL> loadResource(final String location) {
         try {
             return Optional.of(getClass().getResource("resources/" + location));
@@ -40,6 +42,12 @@ public class MediaLoader {
         }
     }
 
+    /**
+     * Private method for loading a {@link FileInputStream} for the requested file.
+     * 
+     * @param location the relative path string within the resources folder
+     * @return the {@link Optional}<{@link FileInputStream}> for the requested file
+     */
     private Optional<FileInputStream> loadFile(final String location) {
         try {
             return Optional.ofNullable(new FileInputStream(new File("resources/" + location)));
@@ -49,28 +57,46 @@ public class MediaLoader {
         }
     }
     
+    /**
+     * Method for retrieving one of the {@link Image} objects defined in {@link ImageType}.
+     * 
+     * @param imageType which image is being requested
+     * @return the {@link Optional}<{@link Image}>
+     */
     public Optional<Image> getImage(final ImageType imageType) {
-        switch(imageType) {
-            case ICON:
-                return this.icon;
-            case MAIN_MENU:
-                return this.mainMenuImage;
-            case SETTINGS_MENU:
-                return this.settingsMenuImage;
-        }
-
-        return Optional.empty();
+        return imageType.image;
     }
 
+    /**
+     * @return the {@link Optional}<{@link Media}> object containing the soundtrack 
+     */
     public Optional<Media> getSoundtrack() {
         return this.soundtrack;
     }
 
+    /**
+     * @return the {@link Optional}<{@link URL}> to the global cascading style sheets file
+     */
     public Optional<URL> getGlobalCssUrl() {
         return this.cssUrl;
     }
 
+    /**
+     * @return the static singleton instance of the {@link MediaLoader} class
+     */
     public static MediaLoader getInstance() {
         return INSTANCE;
     }
+
+    /**
+     * Inner enumeration for the different kinds of images loaded by the {@link MediaLoader} class.
+     */
+    public enum ImageType{
+        ICON,
+        MAIN_MENU,
+        SETTINGS_MENU;
+
+        private Optional<Image> image = Optional.empty();
+    }
+
 }
