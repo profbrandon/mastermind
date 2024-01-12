d98147a01987fa390edeb56a0fd6c5ed2f9078f1
Brandon Harrington <bdharrington0@outlook.com>
2024-01-12 14:09:37 -0600

    added comments to GameState

--- src/GameState.java
+++ src/GameState.java
@@ -4,6 +4,18 @@ import java.util.Optional;
 
 import javafx.util.Pair;
 
+/**
+ * Class to represent the state of a game of {@link Mastermind}. Can be exported as a string
+ * of bytes in the format:
+ * 
+ *            1 byte:  # of slots,
+ *            1 byte:  # of colors,
+ *            1 byte:  # of rows,
+ *      (slots) bytes: solution pegs,
+ * (slots*rows) bytes: guess pegs,
+ * 
+ * which are then compressed down from the total number of bytes (n) to (ceil(n/2)).
+ */
 public class GameState {
     private static final int DEFAULT_SLOTS  = 4;
     private static final int DEFAULT_COLORS = 6;
@@ -17,14 +29,31 @@ public class GameState {
 
     private Row solution;
 
+    /**
+     * Default constructor that creates a game with 4, 6 colors, 8 rows and a random solution code.
+     */
     public GameState() {
         this(GameState.randomSolution(DEFAULT_SLOTS, DEFAULT_COLORS));
     }
 
+    /**
+     * Constructor that creates a default game but with the specified solution.
+     * 
+     * @param solutionPegs the byte array representing the solution
+     */
     public GameState(final byte[] solutionPegs) {
         this(DEFAULT_SLOTS, DEFAULT_COLORS, DEFAULT_ROWS, solutionPegs);
     }
 
+    /**
+     * Constructor that creates a game with the specified number of slots, colors, rows, and with
+     * the specified solution.
+     * 
+     * @param slots the number of slots
+     * @param colors the number of colors
+     * @param maxRows the number of rows
+     * @param solutionPegs the byte array representing the solution
+     */
     public GameState(final int slots, final int colors, final int maxRows, final byte[] solutionPegs) {
         this.slots  = Math.min(Math.max(slots, 2), 10);
         this.colors = Math.min(Math.max(colors, 2), Peg.PegColor.values().length);
@@ -33,17 +62,29 @@ public class GameState {
         this.rows     = new ArrayList<>(this.maxRows);
         this.solution = new Row(solutionPegs, this.slots, false);
 
+        // Initialize all rows to be empty and uneditable
         for (int i = 0; i < this.maxRows; ++i) {
             this.rows.add(new Row(new byte[this.slots], this.slots, false));
         }
 
+        // Only the first row should be initially editable
         this.rows.get(0).toggleEditable();
     }
 
+    /**
+     * @param i the row to select
+     * @return whether the row is full of {@link Peg}s (returns false if the row is undefined)
+     */
     public boolean isRowFull(final int i) {
         return this.getRow(i).map(Row::isFull).orElse(false);
     }
 
+    /**
+     * Sets the current solution of this {@link GameState} object to the specified solution.
+     * 
+     * @param solution the byte array representing the solution
+     * @return whether the method was successful
+     */
     public boolean setSolution(final byte[] solution) {
         final Row row = new Row(solution, this.slots, false);
         if (row.isFull()) {
@@ -54,6 +95,11 @@ public class GameState {
         }
     }
 
+    /**
+     * @param i the ith row coordinate
+     * @param j the jth column coordinate
+     * @return the {@link Optional}<{@link Peg}> at row i and column j (if it exists)
+     */
     public Optional<Peg> pegAt(final int i, final int j) {
         if (i >= rows.size() || i < 0) {
             return Optional.empty();
@@ -62,6 +108,15 @@ public class GameState {
         }
     }
 
+    /**
+     * Computes the feedback for a given row. Feedback consists of two numbers, "red" and "white".
+     * The "red" number records how many {@link Peg}s in the given row are the correct color and in the
+     * correct position. The "white" number records how many {@link Peg}s are the correct color but in the
+     * wrong position.
+     * 
+     * @param i the ith row coordinate
+     * @return a pair of integers (r,w) representing the red and white responses
+     */
     public Pair<Integer, Integer> testRow(final int i) {
         final Pair<Integer, Integer> fail = new Pair<Integer,Integer>(0, 0);
         final Optional<Row> temp = this.getRow(i);
@@ -81,6 +136,14 @@ public class GameState {
         }
     }
 
+    /**
+     * Sets the {@link Peg} at the specified location to be the provided {@link Peg}. 
+     * 
+     * @param i the ith row
+     * @param j the jth column
+     * @param peg the peg to put in this location
+     * @return whether the set was successful
+     */
     public boolean setPeg(final int i, final int j, final Peg peg) {
         if (i < 0 || i >= maxRows) return false;
         else {
@@ -88,6 +151,13 @@ public class GameState {
         }
     }
 
+    /**
+     * Removes any {@link Peg} at the given location.
+     * 
+     * @param i the ith row
+     * @param j the jth column
+     * @return whether the removal was successful
+     */
     public boolean clearPeg(final int i, final int j) {
         if (i < 0 || i >= maxRows) return false;
         else {
@@ -95,6 +165,11 @@ public class GameState {
         }
     }
 
+    /**
+     * Interprets this {@link GameState} object as a list of bytes.
+     * 
+     * @return the {@link List}<{@link Byte}> representation of this object
+     */
     public List<Byte> toByteList() {
         final List<Byte> data = new ArrayList<>();
 
@@ -115,6 +190,9 @@ public class GameState {
         return data;
     }
 
+    /**
+     * Shifts which row is editable to the next row if possible.
+     */
     public void nextRowIfPossible() {
         boolean found = false;
 
@@ -130,6 +208,12 @@ public class GameState {
         }
     }
 
+    /**
+     * Uses the characters specified for each type of {@link Peg} to create a string
+     * representation of this object. Uses "-" characters for empty slots.
+     * 
+     * @return a {@link String} representation of this object
+     */
     @Override
     public String toString() {
         String str = "";
@@ -141,6 +225,13 @@ public class GameState {
         return str;
     }
 
+    /**
+     * Retrieves the row at the given location (or an empty {@link Optional} if the row
+     * does not exist).
+     * 
+     * @param i the ith row
+     * @return the {@link Optional}<{@link Row}>
+     */
     private Optional<Row> getRow(final int i) {
         if (i < 0 || i >= maxRows) return Optional.empty();
         else {
@@ -148,6 +239,16 @@ public class GameState {
         }
     }
 
+    /**
+     * Assumes the given list of bytes consists only of bytes where the top four bits
+     * are zeros and squeezes the {@link List} of {@link Byte}s such that for two
+     * consecutive bytes, the second byte is shifted and put into the upper four bits
+     * of the last byte. This reduces the data by a factor of 2 (after an additional
+     * byte is added to make an even number of input bytes).
+     * 
+     * @param data the data to squeeze
+     * @return the data shrunk by a factor of n -> ceil(n/2)
+     */
     public static List<Byte> squeeze(final List<Byte> data) {
         if (data.size() % 2 != 0) {
             data.add((byte) 0);
@@ -162,6 +263,12 @@ public class GameState {
         return newData;
     }
 
+    /**
+     * Performs the inverse operation to the {@link GameState#squeeze(List)} method.
+     * 
+     * @param data the data to unsqueeze
+     * @return the data expanded by a factor of k -> 2 * k (optionally - 1)
+     */
     public static List<Byte> unsqueeze(final List<Byte> data) {
         final List<Byte> newData = new ArrayList<>();
 
@@ -175,6 +282,13 @@ public class GameState {
         return newData;
     }
 
+    /**
+     * Takes a {@link List} of {@link Byte}s and builds a GameState object from the given
+     * data. This method assumes that the data follows the format given in {@link GameState}.
+     * 
+     * @param data the data to build a {@link GameState} object from
+     * @return the {@link GameState} object
+     */
     public static GameState fromByteList(final List<Byte> data) {
         final int slots   = (int) data.remove(0);
         final int colors  = (int) data.remove(0);
@@ -223,6 +337,13 @@ public class GameState {
         return state;
     }
 
+    /**
+     * Builds a byte array containing a random set of solution {@link Pegs} (as bytes).
+     * 
+     * @param slots the number of slots
+     * @param colors the number of colors
+     * @return the random solution
+     */
     public static byte[] randomSolution(final int slots, final int colors) {
         final byte[] data = new byte[slots];
 
@@ -233,11 +354,23 @@ public class GameState {
         return data;
     }
 
+    /**
+     * Private class for representing a row of {@link Pegs}.
+     */
     private class Row {
         private boolean isEditable;
 
         private final ArrayList<Optional<Peg>> pegs;
         
+        /**
+         * Constructs a {@link Row} from the given byte array of pegs with the given number of slots
+         * and sets the editability.
+         * 
+         * @param pegBytes the byte array to derive peg information from
+         * @param slots the number of slots present in the row
+         * @param isEditable whether the row should be editable via a call to {@link Row#setPeg(int, Peg)} or
+         *        {@link Row#clearPeg(int)}
+         */
         public Row(final byte[] pegBytes, final int slots, final boolean isEditable) {
             this.pegs = new ArrayList<>(slots);
             this.isEditable = isEditable;
@@ -247,6 +380,13 @@ public class GameState {
             }
         }
 
+        /**
+         * Retrieves the {@link Peg} at the given column coordinate. Returns an empty optional if the
+         * coordinate is invalid.
+         * 
+         * @param j the jth column
+         * @return the {@link Optional}<{@link Peg}>
+         */
         public Optional<Peg> pegAt(final int j) {
             if (j >= slots || j < 0) {
                 return Optional.empty();
@@ -255,6 +395,13 @@ public class GameState {
             }
         }
 
+        /**
+         * Sets the {@link Peg} at the given column coordinate.
+         * 
+         * @param j the jth column
+         * @param peg the {@link Peg} to place
+         * @return whether the placement was successful
+         */
         public boolean setPeg(final int j, final Peg peg) {
             if (j >= slots || j < 0) return false;
             else if (isEditable) {
@@ -265,6 +412,12 @@ public class GameState {
             }
         }
 
+        /**
+         * Removes the {@link Peg} at the given column coordinate.
+         * 
+         * @param j the jth column
+         * @return whether the removal was successful
+         */
         public boolean clearPeg(final int j) {
             if (j >= slots || j < 0) return false;
             else if (isEditable) {
@@ -275,6 +428,9 @@ public class GameState {
             }
         }
 
+        /**
+         * @return whether this {@link Row} is full
+         */
         public boolean isFull() {
             for (int i = 0; i < slots; ++i) {
                 if (pegs.get(i).isEmpty()) return false;
@@ -283,11 +439,18 @@ public class GameState {
             return true;
         }
 
-        public int getRed(final Row row) {
+        /**
+         * Computes how many {@link Peg}s are the correct color and in the correct position
+         * given the provided solution {@link Row}.
+         * 
+         * @param solution the solution {@link Row} to test against
+         * @return the number of correct (red) {@link Peg}s
+         */
+        public int getRed(final Row solution) {
             int counter = 0;
             
             for (int i = 0; i < slots; ++i) {
-                final Optional<Peg> p1 = row.pegs.get(i);
+                final Optional<Peg> p1 = solution.pegs.get(i);
                 final Optional<Peg> p2 = this.pegs.get(i);
 
                 if (p1.isPresent() && p2.isPresent()) {
@@ -300,9 +463,16 @@ public class GameState {
             return counter;
         }
 
-        public int getWhite(final Row row) {
+        /**
+         * Computes how many {@link Peg}s are the correct color. This method does not care where the
+         * pegs are, and so it also includes the "red" pegs.
+         * 
+         * @param solution the solution {@link Row} to test against 
+         * @return how many {@link Peg}s are the correct color.
+         */
+        public int getWhite(final Row solution) {
             final int[] colors1 = this.getColorCount();
-            final int[] colors2 = row.getColorCount();
+            final int[] colors2 = solution.getColorCount();
 
             int counter = 0;
 
@@ -313,18 +483,38 @@ public class GameState {
             return counter;
         }
 
+        /**
+         * Toggles the editablility of this {@link Row}. (Determines whether valid calls to {@link Row#setPeg(int, Peg)} 
+         * and {@link Row#clearPeg(int)} are successful.)
+         */
         public void toggleEditable() {
             this.isEditable = !this.isEditable;
         }
 
+        /**
+         * Sets the editablility of this {@link Row}. (Determines whether valid calls to {@link Row#setPeg(int, Peg)} 
+         * and {@link Row#clearPeg(int)} are successful.)
+         * 
+         * @param isEditable whether this {@link Row} should accept edits
+         */
         public void setEditable(final boolean isEditable) {
             this.isEditable = isEditable;
         }
 
+        /**
+         * @return whether this row is currently editable
+         */
         public boolean isEditable() {
             return this.isEditable;
         }
 
+        /**
+         * Computes an array of integers with a length equal to the number of {@link Peg.PegColor}s.
+         * The array is then stocked with the number of {@link Peg}s of that {@link Peg.PegColor} present
+         * in this row.
+         * 
+         * @return the integer array of color counts
+         */
         private int[] getColorCount() {
             final int size = Peg.PegColor.values().length;
 
@@ -343,6 +533,11 @@ public class GameState {
             return data;
         }
 
+        /**
+         * Interprets this {@link Row} as a list of bytes.
+         * 
+         * @return the {@link List}<{@link Byte}> representation of this {@link Row}
+         */
         public List<Byte> toByteList() {
             final List<Byte> byteList = new ArrayList<>(slots);
 
@@ -353,6 +548,12 @@ public class GameState {
             return byteList;
         }
     
+        /**
+         * Uses the characters specified for each type of {@link Peg} to create a string
+         * representation of this object. Uses "-" characters for empty slots.
+         * 
+         * @return a {@link String} representation of this object
+         */
         @Override
         public String toString() {
             String str = "";
